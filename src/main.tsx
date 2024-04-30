be15fd427eea19a89972df415ee446146f529a00
BryanGordon <bryanbge92@gmail.com>
2024-04-29 21:13:33 -0500

    Fix build bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider, createTheme } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
 
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
